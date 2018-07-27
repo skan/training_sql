f19936dba48ed1e304a0708fd3714f3bb484b3a8
“hamzas” <skander.hamza@st.com>
2018-07-27 19:24:23 +0100

    refactor

--- lab2/requetes.sql
+++ lab2/requetes.sql
@@ -5,18 +5,12 @@ SELECT DATE_FORMAT(date_publication, '%d/%m/%Y') AS DATE, pseudo,titre, resume, 
     GROUP BY article.id
     ORDER BY date_publication ASC;
 
-/*Il faut la date de publication (format “12 octobre ‘14” - attention à l’apostrophe devant l’année à deux chiffres), 
-pseudo de l’auteur, titre et résumé de chaque article (triés du plus récent au plus vieux) écrits par l’auteur 2.*/
-
 SELECT DATE_FORMAT(date_publication, '%d %M \'%y') AS DATE, pseudo,titre,resume
     FROM article 
     INNER JOIN utilisateur ON utilisateur.id = auteur_id 
     WHERE auteur_id = 2
     ORDER BY date_publication ASC;
  
- /* Categorie - id de la catégorie = 3
-Il faut la date de publication (format “12/10/2014 - 17:47”), pseudo de l’auteur, titre et résumé de chaque article (triés du plus récent au plus vieux) de la catégorie 3.'*/
-
 SELECT DATE_FORMAT(date_publication, '%d/%m/%Y - %H:%i') AS DATE, pseudo,titre, resume
     FROM article
     INNER JOIN utilisateur ON utilisateur.id = auteur_id 
@@ -24,11 +18,7 @@ SELECT DATE_FORMAT(date_publication, '%d/%m/%Y - %H:%i') AS DATE, pseudo,titre, 
     WHERE categorie_id = 3
     ORDER BY date_publication ASC;
 
-/*Article - id de l’article = 4
-On a déjà les commentaires. Il manque donc la requête pour récupérer 
-la date de publication (format “12 octobre 2014 à 17 heures 47”), le titre, le contenu, les noms des catégories de l’article 4, ainsi que le pseudo de son auteur.*/
-
-SELECT DATE_FORMAT(date_publication, '%d %M %Y a %H heures %i') AS DATE, titre, pseudo, GROUP_CONCAT(categorie.nom) as categorie
+SELECT DATE_FORMAT(date_publication, '%d %M %Y a %H heures %i') AS DATE, titre,contenu, GROUP_CONCAT(categorie.nom) as categorie, pseudo
     FROM article
     INNER JOIN utilisateur ON utilisateur.id = auteur_id 
     RIGHT JOIN categorie_article ON article.id = categorie_article.article_id
