c8e78565e1446fbd6446b1209e38d529db28b83e
“hamzas” <skander.hamza@st.com>
2018-07-27 19:20:58 +0100

    /*Article - id de l’article = 4

--- lab2/requetes.sql
+++ lab2/requetes.sql
@@ -23,3 +23,16 @@ SELECT DATE_FORMAT(date_publication, '%d/%m/%Y - %H:%i') AS DATE, pseudo,titre, 
     RIGHT JOIN categorie_article ON article.id = categorie_article.article_id
     WHERE categorie_id = 3
     ORDER BY date_publication ASC;
+
+/*Article - id de l’article = 4
+On a déjà les commentaires. Il manque donc la requête pour récupérer 
+la date de publication (format “12 octobre 2014 à 17 heures 47”), le titre, le contenu, les noms des catégories de l’article 4, ainsi que le pseudo de son auteur.*/
+
+SELECT DATE_FORMAT(date_publication, '%d %M %Y a %H heures %i') AS DATE, titre, pseudo, GROUP_CONCAT(categorie.nom) as categorie
+    FROM article
+    INNER JOIN utilisateur ON utilisateur.id = auteur_id 
+    RIGHT JOIN categorie_article ON article.id = categorie_article.article_id
+    RIGHT JOIN categorie ON categorie.id = categorie_article.categorie_id
+    WHERE article.id = 4
+    ORDER BY date_publication ASC;
+
