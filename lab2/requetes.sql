5e758e5e16ae1f021efb6b1c9595a3689392aba4
“hamzas” <skander.hamza@st.com>
2018-07-27 18:51:39 +0100

    accueil done

--- lab2/requetes.sql
+++ lab2/requetes.sql
@@ -1,4 +1,6 @@
-select date_format(date_publication, '%d/%m/%Y') as date, pseudo,titre, resume 
-    from article 
-    inner join utilisateur on utilisateur.id = auteur_id 
-    order by date_publication asc;
+SELECT DATE_FORMAT(date_publication, '%d/%m/%Y') AS DATE, pseudo,titre, count(commentaire.id) as nombre_commentaire
+    FROM article 
+    INNER JOIN utilisateur ON utilisateur.id = auteur_id 
+    RIGHT JOIN commentaire ON article.id = article_id
+    GROUP BY article.id
+    ORDER BY date_publication ASC;
